1d294e88474090de2525c67d6d06d80eb7e533eb
george <george.maxi@ucsp.edu.pe>
2026-07-03 21:33:22 -0500

    response entity

--- src/main/java/com/codigo/spring_g17/controller/UsuarioController.java
+++ src/main/java/com/codigo/spring_g17/controller/UsuarioController.java
@@ -5,10 +5,11 @@ import com.codigo.spring_g17.dto.ouput.UsuarioCreateResponse;
 import com.codigo.spring_g17.entity.UsuarioEntity;
 import com.codigo.spring_g17.repository.UsuarioRepository;
 import com.codigo.spring_g17.utils.PasswordGenerator;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -21,10 +22,10 @@ public class UsuarioController {
     }
 
     @PostMapping("/save")
-    public UsuarioCreateResponse crearUsuario(@RequestBody UsuarioCreateDto usuarioCreateDto) {
+    public ResponseEntity<UsuarioCreateResponse> crearUsuario(@RequestBody UsuarioCreateDto usuarioCreateDto) {
         String dni = usuarioCreateDto.getDni();
         if(dni.length() != 8) {
-            return null;
+            return ResponseEntity.badRequest().build();
         }
         // generar contrasenia para el usuario nuevo
         String password = PasswordGenerator.generatePassword(
@@ -43,13 +44,33 @@ public class UsuarioController {
 
         usuarioRepository.save(usuarioEntity);
 
-        return new UsuarioCreateResponse(
+        return ResponseEntity.ok(new UsuarioCreateResponse(
                 usuarioEntity.getIdUsuario(),
                 usuarioEntity.getUsername(),
                 usuarioEntity.getNombre(),
                 usuarioEntity.getApellido(),
                 usuarioEntity.getDni(),
                 usuarioEntity.isEstado()
-        );
+        ));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioCreateResponse> findById(@PathVariable UUID id) {
+        Optional<UsuarioEntity> optionalUsuario = usuarioRepository.findById(id);
+        if(optionalUsuario.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        UsuarioEntity usuarioEntity = optionalUsuario.get();
+        UsuarioCreateResponse usuarioCreateResponse = new UsuarioCreateResponse(
+                usuarioEntity.getIdUsuario(),
+                usuarioEntity.getUsername(),
+                usuarioEntity.getNombre(),
+                usuarioEntity.getApellido(),
+                usuarioEntity.getDni(),
+                usuarioEntity.isEstado()
+                );
+        return ResponseEntity.ok(usuarioCreateResponse);
+    }
+
+    // retornar un usuario por su id
 }
