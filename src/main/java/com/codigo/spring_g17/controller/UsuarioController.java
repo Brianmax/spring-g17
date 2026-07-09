7aeb79c2494063f18403da05e7c0b3e3009c6807
george <george.maxi@ucsp.edu.pe>
2026-07-08 22:41:15 -0500

    agregar role a usuario

--- src/main/java/com/codigo/spring_g17/controller/UsuarioController.java
+++ src/main/java/com/codigo/spring_g17/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import com.codigo.spring_g17.dto.input.UsuarioCreateDto;
 import com.codigo.spring_g17.dto.ouput.UsuarioResponse;
 import com.codigo.spring_g17.service.UsuarioService;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,6 +36,17 @@ public class UsuarioController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(userResponse);
+    }
+
+    @PostMapping("/usuario/role")
+    public ResponseEntity<UsuarioResponse> addRoleToUser(
+            @RequestParam String role, @RequestParam UUID usuarioId
+    ) {
+        UsuarioResponse usuarioResponse = usuarioService.agregarRoleUsuario(role, usuarioId);
+        if(usuarioResponse == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(usuarioResponse);
     }
 
     // retornar un usuario por su id
