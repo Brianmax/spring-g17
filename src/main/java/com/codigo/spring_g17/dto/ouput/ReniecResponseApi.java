f1684b314eae89f83046f0a6dd444fe5d171e4ab
george <george.maxi@ucsp.edu.pe>
2026-07-14 06:54:23 -0500

    feign client

--- src/main/java/com/codigo/spring_g17/dto/ouput/ReniecResponseApi.java
+++ src/main/java/com/codigo/spring_g17/dto/ouput/ReniecResponseApi.java
@@ -1,19 +1,18 @@
 package com.codigo.spring_g17.dto.ouput;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import lombok.Getter;
 import lombok.Setter;
+import tools.jackson.databind.PropertyNamingStrategies;
+import tools.jackson.databind.annotation.JsonNaming;
 
 @Getter
 @Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ReniecResponseApi {
-    @JsonProperty(namespace = "first_name")
     private String firstName;
-    @JsonProperty(namespace = "first_last_name")
     private String firstLastName;
-    @JsonProperty(namespace = "second_last_name")
     private String secondLastName;
-    @JsonProperty(namespace = "full_name")
     private String fullName;
-    @JsonProperty(namespace = "document_number")
     private String documentNumber;
 }
